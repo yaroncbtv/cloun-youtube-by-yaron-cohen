b866090e66f8728b69022ff61218d9f50cf3c7fc
Yaron <45236471+yaroncbtv@users.noreply.github.com>
2020-02-01 20:00:41 +0200

    Add files via upload

---
+++ public_html/precache-manifest.15b3263d35263f196ae80da82c01d0c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f25a45c753be07896c0315c82dd82ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bdada0116477c36f581",
+    "url": "/static/css/main.00d8ae19.chunk.css"
+  },
+  {
+    "revision": "597d8af9899bcff6160a",
+    "url": "/static/js/2.7c1c9a1b.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.7c1c9a1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bdada0116477c36f581",
+    "url": "/static/js/main.1aac1ab7.chunk.js"
+  },
+  {
+    "revision": "eb2ee1b0aa1aa3f780fa",
+    "url": "/static/js/runtime-main.26d12b15.js"
+  }
+]);
